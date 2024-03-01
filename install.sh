360a1f0b8bbd3231398696b38a4b812603c77558
nami <mn19172@stu.tomakomai-ct.ac.jp>
2024-03-01 10:08:45 +0900

    add installer

---
+++ install.sh
@@ -0,0 +1,37 @@
+case  `uname -s` in 
+	Darwin)
+		files=()
+		for file in .*
+		do
+			if [[ $file != '.' && $file != ".." && $file != ".git" &&  $file != "install.sh" ]]
+			then
+				files+=($file)
+			fi
+		done
+		;;
+esac
+
+echo "${files[@]}"
+
+while true
+do
+	read -p "Are you sure you want to create symbolic links for the above files in the home directory? [y/N]:" answer
+
+	case "$answer" in 
+		[yY]*)
+			for file in "${files[@]}"
+			do
+				ln -snfv $PWD/$file $HOME
+			done
+			break
+			;;
+
+		[nN]*)
+			break
+			;;
+
+		*)
+			continue
+			;;	
+	esac
+done
