7ce96ac8ccad0def59e45fa699d4aae09cb5dbc0
nami <nami.minami08121519@icloud.com>
2024-09-07 15:02:46 +0900

    fix indent

--- install.sh
+++ install.sh
@@ -1,48 +1,48 @@
 case `uname -s` in 
-        Darwin)
-                files=()
-                for file in .*
-                do
-                        if [[ $file != '.' && $file != ".." && $file != ".git" &&  $file != "install.sh" ]]
-                        then
-                                files+=($file)
-                        fi
-                done
-                ;;
+    Darwin)
+        files=()
+        for file in .*
+        do
+            if [[ $file != '.' && $file != ".." && $file != ".git" &&  $file != "install.sh" ]]
+            then
+                    files+=($file)
+            fi
+        done
+        ;;
 
-        Linux)
-                files=()
-                for file in .*
-                do
-                        if [[ $file != '.' && $file != ".." && $file != ".git" &&  $file != "install.sh" && $file != ".config/iterm2" && $file != ".matplotlib" ]]
-                        then
-                                files+=($file)
-                        fi
-                done
-                ;;
+    Linux)
+        files=()
+        for file in .*
+        do
+            if [[ $file != '.' && $file != ".." && $file != ".git" &&  $file != "install.sh" && $file != ".config/iterm2" && $file != ".matplotlib" ]]
+            then
+                    files+=($file)
+            fi
+        done
+        ;;
 esac
 
 echo ${files[@]}
 
 while true
 do
-        read -p "Are you sure you want to create symbolic links for the above files in the home directory? [y/N]:" answer
+    read -p "Are you sure you want to create symbolic links for the above files in the home directory? [y/N]:" answer
 
-        case "$answer" in 
-                [yY]*)
-                        for file in "${files[@]}"
-                        do
-                                ln -snfv $PWD/$file $HOME
-                        done
-                        break
-                        ;;
+    case "$answer" in 
+        [yY]*)
+            for file in "${files[@]}"
+            do
+                    ln -snfv $PWD/$file $HOME
+            done
+            break
+            ;;
 
-                [nN]*)
-                        break
-                        ;;
+        [nN]*)
+            break
+            ;;
 
-                *)
-                        continue
-                        ;;      
-        esac
+        *)
+            continue
+            ;;      
+esac
 done
