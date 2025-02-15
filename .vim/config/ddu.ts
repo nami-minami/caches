bde9b3c5270b0f6d20c70b605cd72bbcffb01b71
nami <minami.nami08121519@icloud.com>
2025-02-16 02:58:39 +0900

    first commit ddu

---
+++ .vim/config/ddu.ts
@@ -0,0 +1,13 @@
+import {
+  type ActionArguments,
+  ActionFlags,
+  type DduOptions,
+} from "jsr:@shougo/ddu-vim@~9.5.0/types";
+import {
+  BaseConfig,
+  type ConfigArguments,
+} from "jsr:@shougo/ddu-vim@~9.5.0/config";
+
+import type { Denops } from "jsr:@denops/std@~7.4.0";
+import * as fn from "jsr:@denops/std@~7.4.0/function";
+
