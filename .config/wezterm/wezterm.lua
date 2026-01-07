cebcef89ed408544b89af83601eea576ab817340
nami-minami <minami.nami08121519@icloud.com>
2026-01-08 08:43:33 +0900

    fix

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,8 +1,11 @@
 local wezterm = require 'wezterm';
 local config = wezterm.config_builder();
 
-return {
-    color_scheme = 'Solarized Dark (Gogh)',
-    use_ime = true,
-    macos_forward_to_ime_modifier_mask = 'SHIFT|CTRL',
-}
+config.color_scheme = 'Solarized Dark (Gogh)';
+config.use_ime = true;
+config.macos_forward_to_ime_modifier_mask = 'SHIFT|CTRL';
+config.font_size = 12.0;
+config.window_decorations = 'RESIZE';
+
+return config;
+
